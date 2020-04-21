8fa269dfbfc0a7188d44b83246fd69221bffe4b1
qincy <353713885@qq.com>
2020-04-21 17:54:26 +0800

    不加锁 demo

---
+++ src/main/java/com/example/springaop/service/StockService.java
@@ -0,0 +1,30 @@
+package com.example.springaop.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 模拟库存
+ */
+@Slf4j
+public class StockService {
+    //定义库存总数为 100 （static修饰）
+    public static int stockAmount = 1000;
+
+    private static StockService stockService = new StockService();
+
+    public void addStock() {
+        stockAmount++;
+        log.info("加库存成功,线程名称{}，库存数是{}", Thread.currentThread().getName(), stockAmount);
+    }
+
+    public void subtractStock() {
+        stockAmount--;
+        log.info("减库存成功,线程名称{}--操作成功，库存数是{}", Thread.currentThread().getName(), stockAmount);
+    }
+
+    //单例模式，恶汉模式，线程安全，不会重复创建
+    public static StockService getInstance() {
+        return stockService;
+    }
+
+}
--- src/test/java/com/example/springaop/SpringAopApplicationTests.java
+++ src/test/java/com/example/springaop/SpringAopApplicationTests.java
@@ -1,13 +1,45 @@
 package com.example.springaop;
 
+import com.example.springaop.service.StockService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class SpringAopApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    /**
+     * 测试不加锁
+     */
+    @Test
+    void testOptimisticLock() {
+        for (int i = 0; i < 1000; i++) {
+            new AddThread().start();
+            new  SubtractThread().start();
+        }
+        log.info("库存总数{}",StockService.stockAmount);
+    }
+
+
+    class AddThread extends java.lang.Thread {
+        public void run() {
+            StockService stockService = StockService.getInstance();
+            stockService.addStock();
+        }
+
+    }
+
+    class SubtractThread extends java.lang.Thread {
+        public void run() {
+            StockService stockService = StockService.getInstance();
+            stockService.subtractStock();
+        }
+
     }
 
 }
